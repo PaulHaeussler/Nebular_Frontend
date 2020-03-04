88c90ebe40102088fd31557715a05e455f0c9a2d
Paul Häussler <paul.maximilian.haeussler@students.hs-mainz.de>
2020-03-04 20:21:14 +0900

    test7

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
- // test6
+ // test7
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
